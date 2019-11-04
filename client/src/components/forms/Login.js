8428baec5187bd7280b09700c68a976621f14530
Gregory Simpson <gregorysimpson13@gmail.com>
2019-11-04 14:02:02 -0800

    login screen no longer logs the user out

--- client/src/components/forms/Login.js
+++ client/src/components/forms/Login.js
@@ -16,7 +16,7 @@ export default class Login extends Component {
 
   componentDidMount = () => {
     // if logged in log out
-    logoutUser();
+    // logoutUser();
   };
 
   onSubmit = event => {
