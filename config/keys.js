069bd6bd40d8036af580a6bd495bdf0c42c58bb0
gregorysimpson13 <gregorysimpson13@gmail.com>
2019-11-03 22:11:49 -0800

    Delete keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://gregory1:z^s94P3#@ds027758.mlab.com:27758/financial-tracking-app?retryWrites=true",
-  secretOrKey: "wRF96#fc%2bU45",
-  facebookSecret: "e5c4cab99bd08e4b1a8a91f30bccf494"
-};
