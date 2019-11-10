58e63ee16ab9dd9659176b33487e9b3726fcc13a
Gregory Simpson <gregorysimpson13@gmail.com>
2019-11-10 11:04:27 -0800

    forgot to add the s in headers

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use((req, res, next) => {
   if (process.env.NODE_ENV === "production") {
     if (req.headers["x-forwarded-proto"] !== "https") {
-      return res.redirect("https://" + req.header.host + req.url);
+      return res.redirect("https://" + req.headers.host + req.url);
     } else {
       return next();
     }
