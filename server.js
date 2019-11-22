14354b121704c01f18ff735aa17d506c5aad2ac5
Gregory Simpson <gregorysimpson13@gmail.com>
2019-11-21 23:34:32 -0800

    testing

--- server.js
+++ server.js
@@ -69,4 +69,5 @@ if (process.env.NODE_ENV === "production") {
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
+  console.log(`test hack key is the following ${process.env.hackKey}`);
 });
