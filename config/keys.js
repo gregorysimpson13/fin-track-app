d7c1e0378204b304c03169674184e567fa52c79b
Gregory Simpson <gregorysimpson13@gmail.com>
2019-11-21 23:27:07 -0800

    testing static key

--- config/keys.js
+++ config/keys.js
@@ -3,6 +3,6 @@ module.exports = {
     process.env.mongoURI ||
     "mongodb://gregory1:z^s94P3#@ds027758.mlab.com:27758/financial-tracking-app?retryWrites=true",
   secretOrKey: process.env.secretOrKey || "wRF9lkj&#bU45",
-  facebookSecret: process.env.facebookSecret || "notrealkey",
+  facebookSecret: "e5c4cab99bd08e4b1a8a91f30bccf494", // || "notrealkey",
   facebookClientId: "721703521675195"
 };
